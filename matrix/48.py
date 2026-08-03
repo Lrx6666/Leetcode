ced1a8521d5d945016bb3007bb6f1a9a7ae77e06
Lrx6666 <2727557760@qq.com>
2026-08-03 20:28:13 +0800

    Auto backup hot100

--- matrix/48.py
+++ matrix/48.py
@@ -1,4 +1,13 @@
 ##48 旋转图像
+import copy
+from copy import deepcopy
+
+
 class Solution:
     def rotate(self , matrix : List[List[int]] -> None :
-        
+        n = len(matrix)
+        tmp = deepcopy(matrix)
+
+        for i in range(matrix):
+            for j in range(matrix):
+                matrix[j][n-i-1] = tmp[i][j]
