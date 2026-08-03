4cef1a4255af3442f8b8fd9e575b5e3f1538124a
Lrx6666 <2727557760@qq.com>
2026-08-03 21:17:40 +0800

    Auto backup hot100

--- matrix/240.py
+++ matrix/240.py
@@ -1,9 +1,13 @@
+##240 搜索二维矩阵
 class Solution:
     def searchMatrix(self , matrix : List[List[int]] , target : int) -> bool :
         i = len(matrix) - 1
-        j = len(matrix[0]) - 1
-        while i>=0 and j <=len(matrix[0]):
-            if matrix[i][j] < target : j++
-            if matrix[i][j] > target : i--
-            if matrix[i][j] == target : return True
-            else : return False
+        j = 0
+        while i>=0 and j < len(matrix[0]):
+            if matrix[i][j] < target : j+=1
+            elif matrix[i][j] > target : i-=1
+            else : return True
+        return False
+
+##神之思路不得不跪下来膜拜，应该能记一辈子
+##dai'm
