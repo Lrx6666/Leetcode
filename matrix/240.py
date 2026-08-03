7699962b5fee6166aee20a935e39b7dd7e529e2f
Lrx6666 <2727557760@qq.com>
2026-08-03 21:13:09 +0800

    Auto backup hot100

--- matrix/240.py
+++ matrix/240.py
@@ -1,3 +1,9 @@
 class Solution:
     def searchMatrix(self , matrix : List[List[int]] , target : int) -> bool :
-        
+        i = len(matrix) - 1
+        j = len(matrix[0]) - 1
+        while i>=0 and j <=len(matrix[0]):
+            if matrix[i][j] < target : j++
+            if matrix[i][j] > target : i--
+            if matrix[i][j] == target : return True
+            else : return False
