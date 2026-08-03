05bb9594a4c59872ab763ebb892ff28aeab8dec9
Lrx6666 <2727557760@qq.com>
2026-08-03 20:31:08 +0800

    Auto backup hot100

--- matrix/48.py
+++ matrix/48.py
@@ -4,10 +4,13 @@ from copy import deepcopy
 
 
 class Solution:
-    def rotate(self , matrix : List[List[int]] -> None :
+    def rotate(self, matrix: List[List[int]]) -> None:
         n = len(matrix)
         tmp = deepcopy(matrix)
 
-        for i in range(matrix):
-            for j in range(matrix):
-                matrix[j][n-i-1] = tmp[i][j]
+        for i in range(n):
+            for j in range(n):
+                matrix[j][n - i - 1] = tmp[i][j]
+
+##感觉矩阵的题还挺背板的，记住原地算法用深拷贝这个库deepcopy
+##i j 的规律记住{行换列，列合n}
