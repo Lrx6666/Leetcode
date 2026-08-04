47570a0468bb3028098e6c398f65ab19453c4242
Lrx6666 <2727557760@qq.com>
2026-08-04 21:44:09 +0800

    Auto backup hot100

--- ListNode/234.py
+++ ListNode/234.py
@@ -1,3 +1,8 @@
 ## 234 回文链表
 class ListNode :
-    
+    def __init__ (self . val = 0 , next = None) :
+        self.val = val
+        self.next = next
+class Solution :
+    def isPalindrome(self , head : Optional[ListNode]) -> bool :
+        
