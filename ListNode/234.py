09186f93dd994a7c429d39ad184116dabc771b05
Lrx6666 <2727557760@qq.com>
2026-08-04 21:50:11 +0800

    Auto backup hot100

--- ListNode/234.py
+++ ListNode/234.py
@@ -4,5 +4,10 @@ class ListNode :
         self.val = val
         self.next = next
 class Solution :
+    #找中间节点的函数
+    def middleNode (self , head : Optional[ListNode] ) -> Optional[ListNode] :
+
+
+
+
     def isPalindrome(self , head : Optional[ListNode]) -> bool :
-        
