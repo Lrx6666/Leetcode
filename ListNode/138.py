2fef1ea44b6606a13559b98fee4e9199a2994f70
Lrx6666 <2727557760@qq.com>
2026-08-09 15:48:25 +0800

    Auto backup hot100

--- ListNode/138.py
+++ ListNode/138.py
@@ -14,9 +14,8 @@ class Solution :
                 cur.next.random = cur.random.next #把原来cur.random的next给克隆羊的random
             cur = cur.next.next #跳过克隆羊
         #3 删除交错链表中的原链表节点，剩下的节点即为新链表
-        cur = dummy = Node(0, head)
+        cur = dummy = Node(0 , head) #关键的赋值
         while cur.next :
-            cur = head
             cur.next = cur.next.next #删除原节点
             cur = cur.next
         return dummy.next
---
+++ ListNode/147.py
@@ -0,0 +1,9 @@
+#147
+#排序链表
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
