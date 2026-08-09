87352e5a116e903ce7ef40629ba8aa8e1c077f82
Lrx6666 <2727557760@qq.com>
2026-08-09 16:20:18 +0800

    Auto backup hot100

--- ListNode/148.py
+++ ListNode/148.py
@@ -1,4 +1,4 @@
-#147
+#148
 #排序链表
 # Definition for singly-linked list.
 # class ListNode:
@@ -7,3 +7,14 @@
 #         self.next = next
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or head.next :
+            return head
+        #切断链表
+        slow = head
+        fast = head.next #why???
+        while fast and fast.next :
+            slow = slow.next
+            fast = fast.next.next
+            mid = slow.next
+            slow.next = None
+            
