cfcfddc6c54627d3ce14a895233ae9c331340bf7
Lrx6666 <2727557760@qq.com>
2026-08-09 16:27:24 +0800

    Auto backup hot100

--- ListNode/148.py
+++ ListNode/148.py
@@ -5,6 +5,9 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+from hot100.ListNode.LinkedList import ListNode
+
+
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head or head.next :
@@ -17,10 +20,24 @@ class Solution:
             fast = fast.next.next
         mid = slow.next #后半段第一个
         slow.next = None #前半段最后一个
-        #2 定义前后端链表
+        #2 定义前后段链表
         left = self.sortList(head)
         right = self.sortList(mid)
         #3 合并
-        h = res 
+        cur = ListNode(0)
+        dummy = ListNode(0)
+        while left and right :
+            if left.val < right.val :
+                cur.next = left
+                left = left.next
+            else :
+                cur.next = right
+                right = right.next
+            cur = cur.next
+            if left :
+                cur.next = left
+            else :
+                cur.next = right
+            return dummy.next
 
 
