592844e13c9ebb0f31ceec980c40507ee9ff7cbe
Lrx6666 <2727557760@qq.com>
2026-08-13 22:55:40 +0800

    Auto backup hot100

--- Binary Tree/101.py
+++ Binary Tree/101.py
@@ -8,3 +8,17 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def dfs(right , left) :
+            if not right and not left : return True
+            if not right or not left : return False
+            if left.val != right.val : return False
+            return dfs(left.left, right.right) and \
+                   dfs(left.right, right.left)
+        return dfs(root.right , root.left)
+
+#递归太好用了
+#左右都空 true
+#左右有一个是空 false
+#左右值不等 false
+#往下递归判断
+#
