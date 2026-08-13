6b636ed5a794204bd3b9f93ea901fffa056a5982
Lrx6666 <2727557760@qq.com>
2026-08-13 22:41:02 +0800

    Auto backup hot100

---
+++ Binary Tree/101.py
@@ -0,0 +1,10 @@
+#101
+#对称二叉树
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
