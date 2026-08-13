1cd234de82c2dc8f852cd00ebfdd81e1095bf83e
Lrx6666 <2727557760@qq.com>
2026-08-13 22:39:29 +0800

    Auto backup hot100

--- Binary Tree/226.py
+++ Binary Tree/226.py
@@ -1,2 +1,19 @@
 #226
 #翻转二叉树
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root : return
+        tmp = root.right
+        root.right = self.invertTree(root.left)
+        root.left = self.invertTree(tmp)
+        return root
+
+#递归还蛮简单
+#暂存一边节点 用定义函数翻转 暂存节点给另一半
+#注意root.left = self.invertTree(tmp)这步里面是tmp
